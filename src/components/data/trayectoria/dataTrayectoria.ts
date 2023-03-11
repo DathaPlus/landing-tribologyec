9231154d611fdb451205c396079bd1ad2d037e36
DanPancho <daarcosa@puce.edu.ec>
2023-03-10 20:43:00 -0500

    [IS-01]/feat: cambio gitignore

--- src/components/data/trayectoria/dataTrayectoria.ts
+++ src/components/data/trayectoria/dataTrayectoria.ts
@@ -1,6 +1,6 @@
 import { ITrayectoria } from '@components/interfaces/ITrayectoria';
 
-export const IconsTrayecotria: ITrayectoria = {
+export const trajectoryIcons: ITrayectoria = {
   icons: [
     { src: 'https://www.pikpng.com/pngl/m/493-4936676_eps-teltonika-logo-png-clipart.png' },
     { src: 'https://www.pikpng.com/pngl/m/493-4936676_eps-teltonika-logo-png-clipart.png' },

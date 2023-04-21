9b18ebc41482a74ef138b6edb32b6e3fed16f8d6
DanPancho <daarcosa@puce.edu.ec>
2023-04-21 15:47:45 -0500

    [IS-01]/feat: trayectoria 12 años navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import Bar from '@components/Bar';
 import { Hero, OurProjects, Products, Services } from '@components/home';
 import { Contacts } from '@components/index';
 import React from 'react';
 
 const HomePage = () => (
   <>
+    <Bar />
     <Hero />
     <Services />
     <OurProjects />

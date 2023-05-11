30ed710e61ea4dbdb6d4ab548c3983d1d9826f71
Gt37285 <jbpaig@gmail.com>
2023-05-11 18:12:58 -0500

    [IS-97]/feat: migración componentes a sass

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -6,8 +6,6 @@ import React from 'react';
 
 import Field from './field';
 
-/* eslint-disable no-console */
-
 export const Contacts = () => {
   const width = useLayoutWidth();
 
@@ -18,32 +16,41 @@ export const Contacts = () => {
         backgroundImage: `url('/img/${width < 500 ? 'contact_bg-mobile.png' : 'contact_bg.png'}')`,
       }}
     >
-      <div className="contact__title">
-        <p>
-          {' '}
-          Habla con un <b>experto</b>{' '}
-        </p>
-        <p>
-          ¿Tienes un proyecto en mente? <b>Nosotros</b> te asesoramos
-        </p>
-      </div>
+      <div className="contact__wrapper">
+        <div className="contact__title">
+          <p>
+            {' '}
+            Habla con un <b>experto</b>{' '}
+          </p>
+          <p>
+            ¿Tienes un proyecto en mente? <b>Nosotros</b> te asesoramos
+          </p>
+        </div>
 
-      <Formik
-        initialValues={initialValuesContact}
-        validationSchema={ContactSchema}
-        onSubmit={console.log}
-      >
-        <Form className="contact__form">
-          <Field name="name" />
-          <Field name="phone" />
-          <div className="contact__wrapper-message">
-            <Field name="message" type="textarea" />
-            <Button type="submit" size="normal">
-              Enviar
-            </Button>
-          </div>
-        </Form>
-      </Formik>
+        <Formik
+          initialValues={initialValuesContact}
+          validationSchema={ContactSchema}
+          onSubmit={console.log}
+        >
+          <Form className="contact__form">
+            <div className="contact__wrapper-input">
+              <Field name="name" icon="user" placeholder="Nombre y Apellido" />
+              <Field name="phone" icon="smartphone" placeholder="Número de teléfono" />
+            </div>
+            <div className="contact__wrapper-message">
+              <Field
+                name="message"
+                type="textarea"
+                icon="message-circle"
+                placeholder="Déjanos sun mensaje"
+              />
+              <Button type="submit" size="normal" style="primary">
+                Enviar
+              </Button>
+            </div>
+          </Form>
+        </Formik>
+      </div>
     </section>
   );
 };

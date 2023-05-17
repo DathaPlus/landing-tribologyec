9d00605929cde3f5b802c97376b3bfb489861e5d
Anthony Tacuri <38759855+ant121@users.noreply.github.com>
2023-05-17 16:18:39 -0500

    [AT-140]/feat: add new module for desktop and mobile in home solutions center (#8)

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,4 +1,5 @@
 export * from './hero';
 export * from './services';
 export * from './projects';
+export * from './solutionsCenter';
 export * from './products';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,25 @@
+import React from 'react';
+import { Hero, OurProjects, Products, Services, SolutionsCenter } from '@components/home';
 import Bar from '@components/Bar';
 import { Footer } from '@components/footer';
-import { Hero, OurProjects, Products, Services } from '@components/home';
 import Trayectory from '@components/home/trayectory';
 import { Contacts, Navbar } from '@components/index';
-import React from 'react';
 
-const HomePage = () => (
-  <>
-    <Bar />
-    <Navbar />
-    <Hero />
-    <Services />
-    <OurProjects />
-    <Products />
-    <Trayectory />
-    <Contacts />
-    <Footer />
-  </>
-);
+const HomePage = () => {
+  return (
+    <>
+      <Bar />
+      <Navbar />
+      <Hero />
+      <SolutionsCenter/>
+      <Services />
+      <OurProjects />
+      <Products />
+      <Trayectory />
+      <Contacts />
+      <Footer />
+    </>
+  );
+};
 
 export default HomePage;

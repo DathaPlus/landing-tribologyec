caef5e0f5d11e10fcd9a7b71c2fdb5d6b0566467
Anthony Tacuri <anthony.1211@hotmail.com>
2023-07-26 08:22:06 -0500

    [DT-updateHome]: Fix carrousel project in home.

--- src/components/home/projects/card.tsx
+++ src/components/home/projects/card.tsx
@@ -2,12 +2,16 @@ import { LinkCustom } from '@components/link';
 import { ICardProject } from '@interfaces/home';
 import React, { FC } from 'react';
 
-export const Card: FC<ICardProject> = ({ title, img, link }) => {
+export const Card: FC<ICardProject> = ({ title, img, link, description }) => {
   return (
     <li className="projects__card">
       <LinkCustom {...link}>
-        <div />
+        <div className="projects__card__gradient" />
         <span>{title}</span>
+        <div className="projects__card__information">
+          <p className="projects__card__title">{title}</p>
+          <p className="projects__card__description">{description}</p>
+        </div>
         <img src={img} alt="project" width="100%" height="100%" />
       </LinkCustom>
     </li>
--- src/data/home/project.ts
+++ src/data/home/project.ts
@@ -3,7 +3,8 @@ import { ICardProject } from '@interfaces/home';
 export const listCardsProject: ICardProject[] = [
   {
     img: '/img/projects/project_1.png',
-    title: 'nombre proyecto',
+    title: 'proyecto 1',
+    description: "Multiplicador manual de Torque + Reacción deslizante con doble Inserto Hex de 56 mm.",
     link: {
       href: 'https://www.google.com.ec',
       type: 'external',
@@ -15,7 +16,8 @@ export const listCardsProject: ICardProject[] = [
   },
   {
     img: '/img/projects/project_2.png',
-    title: 'nombre proyecto',
+    title: 'proyecto 2',
+    description: "Multiplicador manual de Torque + Reacción deslizante con doble Inserto Hex de 56 mm.",
     link: {
       href: 'https://www.google.com.ec',
       type: 'external',
@@ -27,7 +29,8 @@ export const listCardsProject: ICardProject[] = [
   },
   {
     img: '/img/projects/project_3.png',
-    title: 'nombre proyecto',
+    title: 'proyecto 3',
+    description: "Multiplicador manual de Torque + Reacción deslizante con doble Inserto Hex de 56 mm.",
     link: {
       href: 'https://www.google.com.ec',
       type: 'external',
@@ -39,7 +42,8 @@ export const listCardsProject: ICardProject[] = [
   },
   {
     img: '/img/projects/project_4.png',
-    title: 'nombre proyecto',
+    title: 'proyecto 4',
+    description: "Multiplicador manual de Torque + Reacción deslizante con doble Inserto Hex de 56 mm.",
     link: {
       href: 'https://www.google.com.ec',
       type: 'external',
@@ -51,7 +55,8 @@ export const listCardsProject: ICardProject[] = [
   },
   {
     img: '/img/projects/project_5.png',
-    title: 'nombre proyecto',
+    title: 'proyecto 5',
+    description: "Multiplicador manual de Torque + Reacción deslizante con doble Inserto Hex de 56 mm.",
     link: {
       href: 'https://www.google.com.ec',
       type: 'external',
@@ -60,5 +65,5 @@ export const listCardsProject: ICardProject[] = [
         rel: 'noreferrer',
       },
     },
-  },
+  }
 ];

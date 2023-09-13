8f8aadddc5e0dd3b1d0fda160087e9992b94d02c
danidaniel6462 <daniel@dathaplus.com>
2023-09-13 12:19:16 -0500

    get data from Wordpress Admin API to services.ts

--- src/components/common/arrows/index.tsx
+++ src/components/common/arrows/index.tsx
@@ -1,25 +1,15 @@
 'use client';
 import React from 'react';
 import { Icon } from '@dathaplus/storybook';
-// TODO: unificar esta interfaz con el Type Props de este archivo
 import { IArrowsCarousel } from '@interfaces/common';
 
-type Props = {
-  type?: 'left' | 'right';
-  id?: string;
-  positionH?: number;
-  top?: number | string;
-  relative?: boolean;
-  className?: string;
-};
-
 export const ArrowsCarousel = ({
-   type = 'left',
-   id,
-   positionH: position = 30,
-   relative = false,
-   top = '50%',
-}: Props) => (
+  type = 'left',
+  id,
+  positionH: position = 30,
+  relative = false,
+  top = '50%',
+}: IArrowsCarousel) => (
   <div
     className={`arrow_carousel arrow_carousel_${type}}`}
     id={id}

ffdce57a5c827ea15cbc4da3e41a69fae539f831
Anthony Tacuri <anthony.1211@hotmail.com>
2023-09-14 16:52:26 -0500

    [IS-377]: Add pagination page

--- src/interfaces/server/common/IGetWordpressPosts.ts
+++ src/interfaces/server/common/IGetWordpressPosts.ts
@@ -1,3 +1,5 @@
 export interface IGetWordpressPageDataParams {
   search?: string;
+  page?: number;
+  perPage?: number;
 }
--- src/server/common/getWordpressPosts.ts
+++ src/server/common/getWordpressPosts.ts
@@ -5,9 +5,13 @@ export const getWordpressPosts = async <ACF = object>(
   params?: IGetWordpressPageDataParams,
 ): Promise<IWordpressPageData<ACF>[] | undefined> => {
   try {
+    const { page = 1, perPage = 9, search } = params || {};
+
     const response = await fetch(
-      `${process.env.BASE_PATH_WORDPRESS_BACKEND}/wp-json/wp/v2/posts${
-        params?.search ? `?search=${params?.search}` : ''
+      `${
+        process.env.BASE_PATH_WORDPRESS_BACKEND
+      }/wp-json/wp/v2/posts?per_page=${perPage}&page=${page}${
+        params?.search ? `&search=${search}` : ''
       }`,
       {
         method: 'GET',

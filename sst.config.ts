c1f9b5e595f78d9f4e762b3d4eb794c19b4b9a6a
danidaniel6462 <daniel@dathaplus.com>
2023-09-17 23:14:24 -0500

    update package-lock.json

--- sst.config.ts
+++ sst.config.ts
@@ -1,24 +1,24 @@
 import { SSTConfig } from 'sst';
 import { App, NextjsSite } from 'sst/constructs';
 import { SsrDomainProps } from 'sst/constructs/SsrSite';
+import { ConfigOptions } from 'sst/project';
 import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
 
 export default {
-  config(_input) {
+  config(_input): Promise<ConfigOptions> | ConfigOptions {
     return {
       name: 'landing-tribologyec',
-      region: 'us-east-1',
+      region: 'us-east-2',
     };
   },
   stacks(app: App) {
-    app.stack(({ stack }) => {
+    app.stack(({ stack }): void => {
       const aws_domain_certificate: SsrDomainProps = {
         isExternalDomain: true,
         domainName: 'www2.tribologyec.com',
         cdk: {
           // @ts-ignore
           certificate: Certificate.fromCertificateArn(
-            // @ts-ignore
             stack,
             'tribologyec_AWS_Certificate',
             'arn:aws:acm:us-east-1:136597708042:certificate/ae3fc89a-529b-418f-8608-58b6428fc6de',

02b24c0bd6877c4adfedcaae4f81b437f5843125
danidaniel6462 <daniel@dathaplus.com>
2024-06-20 13:47:08 -0500

    [DT+] deplyoy

--- sst.config.ts
+++ sst.config.ts
@@ -2,6 +2,46 @@ import { SSTConfig } from 'sst';
 import { App, NextjsSite } from 'sst/constructs';
 import { SsrDomainProps } from 'sst/constructs/SsrSite';
 import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
+
+export default {
+  config(_input) {
+    return {
+      name: 'landing-tribologyec',
+      region: 'us-east-2',
+    };
+  },
+  stacks(app: App) {
+    app.stack(function ({ stack }): void {
+      const aws_domain_certificate: SsrDomainProps = {
+        isExternalDomain: true,
+        domainName: 'www3.tribologyec.com',
+        cdk: {
+          // @ts-ignore
+          certificate: Certificate.fromCertificateArn(
+            stack,
+            'tribologyec_AWS_Certificate',
+            'arn:aws:acm:us-east-1:136597708042:certificate/04e14c48-fab8-4198-baa7-81abc8854067',
+          ),
+        },
+      };
+
+      const site: NextjsSite = new NextjsSite(stack, 'site', {
+        // edge: true,
+        customDomain: aws_domain_certificate,
+      });
+
+      stack.addOutputs({
+        SiteUrl: site.url,
+      });
+    });
+  },
+} satisfies SSTConfig;
+
+/*
+import { SSTConfig } from 'sst';
+import { App, NextjsSite } from 'sst/constructs';
+import { SsrDomainProps } from 'sst/constructs/SsrSite';
+import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
 import { Distribution } from 'aws-cdk-lib/aws-cloudfront';
 
 export default {
@@ -52,3 +92,4 @@ export default {
     });
   },
 } satisfies SSTConfig;
+*/

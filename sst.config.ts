24dee6782ff25e3d5469b0f67bb9cf02aefbaaf5
danidaniel6462 <daniel@dathaplus.com>
2024-06-20 12:57:38 -0500

    [DT+] deplyoy

--- sst.config.ts
+++ sst.config.ts
@@ -2,6 +2,7 @@ import { SSTConfig } from 'sst';
 import { App, NextjsSite } from 'sst/constructs';
 import { SsrDomainProps } from 'sst/constructs/SsrSite';
 import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
+import { Distribution } from 'aws-cdk-lib/aws-cloudfront';
 
 export default {
   config(_input) {
@@ -12,22 +13,32 @@ export default {
   },
   stacks(app: App) {
     app.stack(function ({ stack }): void {
-      const aws_domain_certificate: SsrDomainProps = {
-        isExternalDomain: true,
-        domainName: 'www3.tribologyec.com',
-        cdk: {
-          // @ts-ignore
-          certificate: Certificate.fromCertificateArn(
-            stack,
-            'tribologyec_AWS_Certificate',
-            'arn:aws:acm:us-east-1:136597708042:certificate/04e14c48-fab8-4198-baa7-81abc8854067',
-          ),
-        },
-      };
+      // const aws_domain_certificate: SsrDomainProps = {
+      //   isExternalDomain: true,
+      //   domainName: 'www3.tribologyec.com',
+      //   cdk: {
+      //     // @ts-ignore
+      //     certificate: Certificate.fromCertificateArn(
+      //       stack,
+      //       'tribologyec_AWS_Certificate',
+      //       'arn:aws:acm:us-east-1:136597708042:certificate/04e14c48-fab8-4198-baa7-81abc8854067',
+      //     ),
+      //   },
+      // };
 
+      // @ts-ignore
       const site: NextjsSite = new NextjsSite(stack, 'site', {
         // edge: true,
-        customDomain: aws_domain_certificate,
+        // customDomain: aws_domain_certificate,
+        cdk: {
+          distribution: Distribution.fromDistributionAttributes(
+            stack,
+            "ExistingDistribution",
+            {
+              distributionId: "E37KJ6EEOIGLYM",
+            },
+          ),
+        },
       });
 
       stack.addOutputs({

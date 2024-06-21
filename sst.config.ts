dc4af9301c1bab9e557222061d10855c27115847
danidaniel6462 <daniel@dathaplus.com>
2024-06-21 13:50:12 -0500

    [DT+] deploy

--- sst.config.ts
+++ sst.config.ts
@@ -1,8 +1,6 @@
 import { SSTConfig } from 'sst';
 import { App, NextjsSite } from 'sst/constructs';
-import { SsrDomainProps } from 'sst/constructs/SsrSite';
-import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
-import { Distribution } from 'aws-cdk-lib/aws-cloudfront';
+import { Distribution, IDistribution } from 'aws-cdk-lib/aws-cloudfront';
 
 export default {
   config(_input) {
@@ -13,36 +11,20 @@ export default {
   },
   stacks(app: App) {
     app.stack(function ({ stack }): void {
-      // const aws_domain_certificate: SsrDomainProps = {
-      //   isExternalDomain: true,
-      //   domainName: 'www3.tribologyec.com',
-      //   cdk: {
-      //     // @ts-ignore
-      //     certificate: Certificate.fromCertificateArn(
-      //       stack,
-      //       'tribologyec_AWS_Certificate',
-      //       'arn:aws:acm:us-east-1:136597708042:certificate/04e14c48-fab8-4198-baa7-81abc8854067',
-      //     ),
-      //   },
-      // };
 
-      // @ts-ignore
+      const existingDistribution: IDistribution = Distribution.fromDistributionAttributes(
+          stack,
+          "ExistingDistribution",
+          {
+            distributionId: "E34LRVBAGVE72Y",
+            domainName: "d2auihfh7j1fox.cloudfront.net"
+          },
+        );
+
       const site: NextjsSite = new NextjsSite(stack, 'site', {
-        // edge: true,
-        // customDomain: aws_domain_certificate,
         cdk: {
           // @ts-ignore
-          distribution: Distribution.fromDistributionAttributes(
-            // @ts-ignore
-            stack,
-            "ExistingDistribution",
-            {
-              /*distributionId: "E37KJ6EEOIGLYM",
-              domainName: "d9855tnqnv9vr.cloudfront.net"*/
-              distributionId: "E34LRVBAGVE72Y",
-              domainName: "d2auihfh7j1fox.cloudfront.net"
-            },
-          ),
+          distribution: existingDistribution
         },
       });
 

b28447f0e74ff33ee873368e6b5ac37460df4ba8
Daniel Loza <darwindaniel.dl@gmail.com>
2024-08-26 16:48:31 -0500

    [DT+] deploy (#24)
    Co-authored-by: elvismpq96 <95381416+elvismpq96@users.noreply.github.com>

--- sst.config.ts
+++ sst.config.ts
@@ -1,34 +1,18 @@
 import { SSTConfig } from 'sst';
 import { App, NextjsSite } from 'sst/constructs';
-import { SsrDomainProps } from 'sst/constructs/SsrSite';
-import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
 
 export default {
   config(_input) {
     return {
       name: 'landing-tribologyec',
-      region: 'us-east-2',
+      region: 'us-east-1',
     };
   },
   stacks(app: App) {
     app.stack(function ({ stack }): void {
-      const aws_domain_certificate: SsrDomainProps = {
-        isExternalDomain: true,
-        domainName: 'www3.tribologyec.com',
-        cdk: {
-          // @ts-ignore
-          certificate: Certificate.fromCertificateArn(
-            stack,
-            'tribologyec_AWS_Certificate',
-            'arn:aws:acm:us-east-1:136597708042:certificate/04e14c48-fab8-4198-baa7-81abc8854067',
-          ),
-        },
-      };
 
-      const site: NextjsSite = new NextjsSite(stack, 'site', {
-        // edge: true,
-        customDomain: aws_domain_certificate,
-      });
+
+      const site: NextjsSite = new NextjsSite(stack, 'site');
 
       stack.addOutputs({
         SiteUrl: site.url,

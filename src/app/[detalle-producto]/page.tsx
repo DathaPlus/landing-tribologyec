2495e09ca5c111577fbf7195e8325bb4c5f1706e
Smokefcc <jpc.acosta@yavirac.edu.ec>
2024-08-29 12:39:42 -0500

    merge release

--- src/app/[detalle-producto]/page.tsx
+++ src/app/[detalle-producto]/page.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 import Bar from '@components/Bar';
 import { Footer } from '@components/footer';
 import { Navbar } from '@components/navbar';
-import { Hero } from '@components/base';
-import contactHeroBanner from '@assets/img/contact_hero_bg.png';
-import { getWordpressPageData } from '@server/common/getWordpressPageData';
-import { IContactPage } from '@interfaces/app/IContactPage';
-import { IWordpressPageData } from '@interfaces/server/common/IGetWordpressPageData';
 import { RelatedProducts } from "@components/catalogue/relatedProducts/relatedProducts";
 import { AditionalInfo } from "@components/catalogue/aditionalInformation/aditionalInfo";
 import Evotorque from '@assets/img/tools/03.png';
@@ -29,6 +24,10 @@ export interface IProductDetails {
 }
 
 const DetailProduct = async () => {
+    // const contactData: IWordpressPageData<IContactPage> | undefined =
+    //     await getWordpressPageData<IContactPage>({
+    //         searchParams: { namePage: 'Detalle Producto' },
+    //     });
 
     const productDetails: IProductDetails = {
         weight: "6.8 kg",

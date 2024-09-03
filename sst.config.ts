057cc41594a7554b0a80c183cfad3a4fb4c9c96d
Elvis Pérez <elvmarper@gmail.com>
2024-09-03 15:07:37 -0500

    [DT+] Actualización deploy

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,7 @@
 import { SSTConfig } from 'sst';
 import { App, NextjsSite } from 'sst/constructs';
+import {Certificate, ICertificate} from "aws-cdk-lib/aws-certificatemanager";
+import {SsrDomainProps} from "sst/constructs/SsrSite";
 
 export default {
   config(_input) {
@@ -11,8 +13,24 @@ export default {
   stacks(app: App) {
     app.stack(function ({ stack }): void {
 
+      const arnCertificate =
+          "arn:aws:acm:us-east-1:136597708042:certificate/04e14c48-fab8-4198-baa7-81abc8854067";
 
-      const site: NextjsSite = new NextjsSite(stack, 'site');
+      const aws_domain_certificate: SsrDomainProps = {
+        isExternalDomain: true,
+        domainName: "tribologyec.com",
+        cdk: {
+          // @ts-ignore
+          certificate: Certificate.fromCertificateArn(
+              stack,
+              `tribologyec_${stack.stage}_AWS_Certificate`,
+              arnCertificate,
+          ) as ICertificate,
+        },
+      };
+      const site: NextjsSite = new NextjsSite(stack, 'site',{
+        customDomain: aws_domain_certificate,
+      });
 
       stack.addOutputs({
         SiteUrl: site.url,
